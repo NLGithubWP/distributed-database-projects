237f7a65f824342b2bacef7fee0b54c05b19c306
nailixing <xingnaili14@gmail.com>
2021-09-26 23:27:34 +0800

    add indexs

--- procedures.sql
+++ procedures.sql
@@ -12,4 +12,8 @@
     CREATE INDEX c_joint_id ON customer (c_w_id, c_d_id, c_id);
     CREATE INDEX d_joint_id ON district (d_w_id, d_id);
     CREATE INDEX order_ori_joint_id ON order_ori (o_w_id, o_d_id, o_id, o_carrier_id);
+    CREATE INDEX order_ori_c_id ON order_ori (o_c_id);
+    CREATE INDEX order_ori_entry_d ON order_ori (o_entry_d);
     CREATE INDEX order_line_joint_id ON order_line (ol_w_id, ol_d_id, ol_o_id, ol_number);
+
+
