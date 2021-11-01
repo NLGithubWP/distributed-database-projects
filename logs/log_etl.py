c1dd1db7fdf6e565c1bf9784f5d048ccbccf6a3c
nailixing <xingnaili14@gmail.com>
2021-11-02 02:39:37 +0800

    Update

--- logs/log_etl.py
+++ logs/log_etl.py
@@ -4,8 +4,12 @@
 stat = {}
 total_time = 0
 
+res = []
+mapper = {}
+
+
 for id in range(0, 39):
-    file_name = "errorLogs/tx_log_"+str(id)
+    file_name = "/Users/nailixing/Documents/NUS_Modules/CS5424_Distributed_Database/projects/CS5424/logs/tx_log_"+str(id)
     print(file_name)
     with open(file_name+".txt", "r") as f:
         line_contents = f.readlines()
@@ -19,28 +23,14 @@ for id in range(0, 39):
 
             if " total time used: " in line_content:
                 total_time += float(line_content.split(" ")[-3])
+                res.append(float(line_content.split(" ")[-3]))
+                if file_name not in stat:
+                    mapper[float(line_content.split(" ")[-3])] = [file_name]
+                else:
+                    mapper[float(line_content.split(" ")[-3])].append(file_name)
 
 
-print(stat)
-print("total-time: ", total_time)
-print("throughput: ", 900*39/total_time)
-print("average latency: ", total_time/(900*39))
-
-
-
-'''
-最新的
-{' NewOrderTxParams': 49, ' PaymentTxParams': 59, ' DeliveryTxParams-1': 1, ' DeliveryTxParams-9': 1, ' StockLevelTxParams': 4, ' DeliveryTxParams-3': 1}
-total-time:  96048.34614467621
-throughput:  0.36544096185820196
-average latency:  2.736420118081943
-'''
-
-
-'''
-之前的
-{' RelCustomerTxParams': 29, ' NewOrderTxParams': 69, ' PaymentTxParams': 49, ' DeliveryTxParams-5': 4, ' DeliveryTxParams-3': 2, ' StockLevelTxParams': 3, ' DeliveryTxParams-7': 2, ' DeliveryTxParams-2': 2, ' DeliveryTxParams-4': 1}
-total-time:  87714.9020178318
-throughput:  0.4001600548201539
-average latency:  2.4990000574880855
-'''
+# 打印最快和最慢的client，然后人为地去看文件内的信息
+res.sort()
+print(mapper[res[0]])
+print(mapper[res[-1]])
