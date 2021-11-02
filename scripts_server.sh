b201fed9f2de5f90896c0d6609dae62d62a37f01
nailixing <xingnaili14@gmail.com>
2021-11-02 12:37:18 +0800

    Update

--- scripts_server.sh
+++ scripts_server.sh
@@ -9,7 +9,7 @@ do
         server_id=`expr $i % $server_num`
         server_index=`expr 55 + $server_id`
         echo "$i" "xcnd$server_index:$server_port"
-        xcnd$server_index "python3 cockroachDB_driver.py -u postgresql://naili:@xcnd$server_index:$server_port/cs5424db -p /home/stuproj/cs4224p/temp/tasks/project_files_4/xact_files_A/$i.txt -w A >logs/python$i.log &"
+        ssh xcnd$server_index "python3 cockroachDB_driver.py -u postgresql://naili:@xcnd$server_index:$server_port/cs5424db -p /home/stuproj/cs4224p/temp/tasks/project_files_4/xact_files_A/$i.txt -w A >logs/python$i.log &"
 
         i=$(($i+1))
 done
