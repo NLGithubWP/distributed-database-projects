ff14849474e73a1cbf896581ba06bed2671bde30
nailixing <xingnaili14@gmail.com>
2021-11-02 20:29:47 +0800

    Update

--- sqls/dbinit-workload-A.sql
+++ sqls/dbinit-workload-A.sql
@@ -184,8 +184,9 @@ CREATE TABLE IF NOT EXISTS cs5424db.workloadA.order_line (
     FAMILY freqRead (OL_I_ID, OL_AMOUNT, OL_SUPPLY_W_ID, OL_QUANTITY, OL_DIST_INFO),
     FAMILY freqWrite (pid, ol_w_id, ol_d_id, ol_o_id, ol_number, OL_DELIVERY_D),
     INDEX order_line_joint_id (ol_w_id, ol_d_id, ol_o_id, ol_number),
-    INDEX order_line_i_id(ol_i_id),
-    INDEX order_line_q(OL_QUANTITY));
+    INDEX order_line_i_id (ol_i_id),
+    INDEX order_line_w_id (ol_w_id),
+    INDEX order_line_q (OL_QUANTITY));
 
 IMPORT INTO cs5424db.workloadA.order_line (ol_w_id, ol_d_id, ol_o_id, ol_number, OL_I_ID, OL_DELIVERY_D, OL_AMOUNT, OL_SUPPLY_W_ID, OL_QUANTITY, OL_DIST_INFO)
     CSV DATA ('http://xcnd55:3000/home/stuproj/cs4224p/temp/tasks/project_files_4/data_files/order-line.csv')
